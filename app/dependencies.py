4d19c20d547b8c85698871e63041fc8398101479
chiao7b <86046707+chawkibsa@users.noreply.github.com>
2025-03-19 10:17:49 +0000

    fix: migrate to Postgres

--- app/dependencies.py
+++ app/dependencies.py
@@ -3,7 +3,7 @@ from sqlmodel import SQLModel, create_engine, Session, select
 
 
 # Database setup
-DATABASE_URL = "sqlite:///./test.db"
+DATABASE_URL = "postgresql://orbitundev:orbitundev@localhost/orbitundevdb"
 engine = create_engine(DATABASE_URL, echo=True)
 
 # Database utilities
